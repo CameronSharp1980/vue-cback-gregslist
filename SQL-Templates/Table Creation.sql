d3f175ab0823763fc174ced3223f0977afbde4e2
Cameron Sharp <cameron.sharp1980@gmail.com>
2018-01-08 11:56:05 -0700

    blarg push

--- SQL-Templates/Table Creation.sql
+++ SQL-Templates/Table Creation.sql
@@ -1,11 +1,11 @@
 CREATE TABLE sharpautos (
          id int NOT NULL AUTO_INCREMENT,
-         title VARCHAR(255),
-         make VARCHAR(255),
-         model VARCHAR(255),
-         color VARCHAR(255),
+         title VARCHAR(255) NOT NULL,
+         make VARCHAR(255) NOT NULL,
+         model VARCHAR(255) NOT NULL,
+         color VARCHAR(255) NOT NULL,
          autodescription VARCHAR(255),
          imageurl VARCHAR(255),
-         price DECIMAL,
+         price DECIMAL NOT NULL,
          PRIMARY KEY (id)
       );
